3afed59379bc14f5ac081e00fcc9d9dc0c53d1f9
dipsywong98 <facebookarmyoffcal@gmail.com>
2018-10-17 00:38:21 +0800

    submitted ass2

--- assignment1/collectSubmission.py
+++ assignment1/collectSubmission.py
@@ -1,3 +1,3 @@
 import os
 os.system("del assignment1.7z")
-os.system("7z.exe a -r assignment1 . -xr!*cs231n/datasets* -xr!*ipynb_checkpoints* -xr!*README.md -xr!*collectSubmission* -xr!*requirements.txt -xr!*git* -xr!7z.exe -xr!7z.dll")
+os.system("7z.exe a -r assignment1 . -xr!*cs231n/datasets* -xr!*ipynb_checkpoints* -xr!*README.md -xr!*collectSubmission* -xr!*requirements.txt -xr!*git* -xr!7z.exe -xr!7z.dll -xr!mode*")
